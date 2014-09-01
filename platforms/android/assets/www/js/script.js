c71a4847d3c349f97d5e656730d524804eb20990
Jonnas Fonini <jonnasfonini@gmail.com>
2014-09-01 17:23:07 -0300

    Função para detectar conexão

--- platforms/android/assets/www/js/script.js
+++ platforms/android/assets/www/js/script.js
@@ -3,7 +3,10 @@ var medicos = {
   2: 'Gregory House'
 };
 
-$(document).bind("mobileinit", function() {
+var ipServidor = '127.0.0.1';
+
+// Desabilita transições para melhorar performance
+$(document).bind('mobileinit', function() {
   $.mobile.defaultDialogTransition = 'none';
   $.mobile.defaultPageTransition = 'none';
 });
@@ -14,12 +17,14 @@ $(document).on('pageshow', '#lista-consultas', function() {
   // Requisição AJAX para retornar as consultas já cadastradas
   $.ajax({
     type: 'GET',
-    url: 'http://192.168.1.111/consultas',
+    url: 'http://' + ipServidor + '/consultas',
     dataType: 'json'
   }).success(function(dados) {
 
     // Assim que a requisição retornar as consultas, montamos a listagem
     popularLista(dados);
+
+    $('#btn-salvar').data('action', 'save');
   });
 });
 
@@ -33,6 +38,13 @@ $(document).on('click', '#btn-salvar', function() {
   salvarConsulta();
 });
 
+// Evento: Quando clicar no botão "Editar", chama função que retorna dados do servidor e preenche o formulário
+$(document).on('click', '.editar-consulta', function() {
+  var idConsulta = $(this).data('id');
+
+  editarConsulta(idConsulta);
+});
+
 
 /*
  *Funções
@@ -44,7 +56,7 @@ function popularLista(dados) {
 
   for (var i = 0; i < dados.length; i++) {
     lista += '<li>Médico: ' + medicos[dados[i].medico] + '<br/>Paciente: ' + dados[i].paciente;
-    lista += '<div style="float:right">Data: ' + dados[i].data + '</div></li>';
+    lista += '<div style="float:right">Data: ' + dados[i].data + ' - <a class="editar-consulta" data-id="' + dados[i].id + '">Editar</a></div></li>';
   }
 
   $('#listview-consultas').html(lista).listview('refresh');
@@ -52,7 +64,7 @@ function popularLista(dados) {
 
 // Chamada na página de cadastro para montar o select com os médicos
 function popularSelectMedicos() {
-  var select = '<option selected>Selecione</option>';
+  var select = '<option>Selecione</option>';
 
   for (var i in medicos) {
     select += '<option value="' + i + '">' + medicos[i] + '</option>';
@@ -61,26 +73,82 @@ function popularSelectMedicos() {
   $('#medico').html(select);
 }
 
-function salvarConsulta() {
+function editarConsulta(idConsulta) {
 
-  // Requisição AJAX para salvar a consulta
+  // Requisição AJAX para retornar os dados da consulta
   $.ajax({
-    type: 'POST',
-    url: 'http://192.168.1.111/consultas',
-    data: $('#form-cadastro').serialize(),
+    type: 'GET',
+    url: 'http://' + ipServidor + '/consultas/' + idConsulta,
     dataType: 'json'
-  }).success(function(dados) {
-    alert(dados.status);
+  }).success(function(data) {
+    window.location.hash = '#cadastro-consultas';
 
-    window.location.hash = '#lista-consultas';
+    setTimeout(function() {
+      $('#id').val(data.id);
+
+      setTimeout(function() {
+        $('#medico').val(data.medico).selectmenu('refresh');
+      }, 500);
+
+      $('#paciente').val(data.paciente);
+      $('#data').val(data.data);
+
+      $('#btn-salvar').data('action', 'update');
+    }, 350);
 
   }).fail(function(err) {
-    try {
-      var erro = JSON.parse(err.responseText);
-      alert(erro.status);
-    } catch (e) {
-      console.log(err.responseText);
-    }
+    alert('Erro ao carregar dados');
   });
+}
 
+function salvarConsulta() {
+
+  if ($('#btn-salvar').data('action') == 'save') {
+
+    // Requisição AJAX para salvar a consulta
+    $.ajax({
+      type: 'POST',
+      url: 'http://' + ipServidor + '/consultas',
+      data: $('#form-cadastro').serialize(),
+      dataType: 'json'
+    }).success(function(dados) {
+      alert(dados.status);
+
+      $('#form-cadastro')[0].reset();
+
+      window.location.hash = '#lista-consultas';
+
+    }).fail(function(err) {
+      try {
+        var erro = JSON.parse(err.responseText);
+        alert(erro.status);
+      } catch (e) {
+        console.log(err.responseText);
+      }
+    });
+  } else {
+    // Requisição AJAX para atualizar a consulta
+    $.ajax({
+      type: 'PUT',
+      url: 'http://' + ipServidor + '/consultas',
+      data: $('#form-cadastro').serialize() + '&' + $.param({
+        id: $('#id').val()
+      }),
+      dataType: 'json'
+    }).success(function(dados) {
+      alert(dados.status);
+
+      $('#form-cadastro')[0].reset();
+
+      window.location.hash = '#lista-consultas';
+
+    }).fail(function(err) {
+      try {
+        var erro = JSON.parse(err.responseText);
+        alert(erro.status);
+      } catch (e) {
+        console.log(err.responseText);
+      }
+    });
+  }
 }
